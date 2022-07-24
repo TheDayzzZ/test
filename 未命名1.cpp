da15750017abaad67c3c6d8a4945582825dfa857
TheDayzzZ <45002865+TheDayzzZ@users.noreply.github.com>
2022-07-24 23:12:22 +0800

    Add files via upload

---
+++ 未命名1.cpp
@@ -0,0 +1,7 @@
+//1. 给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+//1->2->3->4 k=2   3->4->1->2->null
+//public ListNode rotateRight(ListNode head, int k) 
+#include<iostream>
+using namespace std;
+ListNode* 
+
